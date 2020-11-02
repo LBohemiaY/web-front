3e13463a06d01f4f8992fa500a9293c9d0f48b62
Motohiro <704398960@qq.com>
2020-11-02 15:46:06 +0800

    添加api，修改登录验证

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/home/Home'
 
+import {Message} from 'element-ui';
+
+import store from '@/store'
+import {getToken} from '@/request/token'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     // 主页
     {
@@ -40,6 +45,53 @@ export default new Router({
       path : '/register',
       name : 'register',
       component : () => import('@/views/common/Register')
-    }
+    },
+    //写文章
+    {
+      path: '/write/:id?',
+      component: () => import('@/views/blog/BlogWrite'),
+      meta: {
+        requireLogin: true
+      },
+    },
+
   ]
 })
+
+/**
+ * router在切换前的token验证
+ * 部分页面必须登录才能进行
+ */
+router.beforeEach((to, from, next) => {
+
+  if (getToken()) {
+
+    if (to.path === '/login') {
+      next({path: '/'})
+    } else {
+      if (store.state.account.length === 0) {
+        store.dispatch('getUserInfo').then(data => { //获取用户信息
+          next()
+        }).catch(() => {
+          next({path: '/'})
+        })
+      } else {
+        next()
+      }
+    }
+  } else {
+    if (to.matched.some(r => r.meta.requireLogin)) {
+      Message({
+        type: 'warning',
+        showClose: true,
+        message: '请先登录哦'
+      })
+
+    }
+    else {
+      next();
+    }
+  }
+})
+
+export default router
