e8fb8850995e289f67dd2b32d83e8ca6affa44d3
Motohiro <704398960@qq.com>
2020-11-02 09:14:59 +0800

    添加view-未完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home'
+import Home from '@/views/home/Home'
 
 Vue.use(Router)
 
@@ -10,7 +10,24 @@ export default new Router({
     {
       path: '',
       name: 'Home',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '/',
+          name: 'homeIndex',
+          component: () => import('@/views/home/HomeIndex')
+        },
+        {
+          path: '/noticeBoard',
+          name: 'noticeBoard',
+          component: () => import('@/views/common/NoticeBoard')
+        },
+        {
+          path: '/log',
+          name: 'log',
+          component: () => import('@/views/common/Log')
+        }
+      ]
     }
   ]
 })
